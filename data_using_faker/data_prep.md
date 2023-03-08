6d61c28af10718302468867f84bbad60f962b150
sunetro123 <20804544+sunetro123@users.noreply.github.com>
2023-03-08 19:38:38 +0000

    First success with faker

--- data_using_faker/data_prep.md
+++ data_using_faker/data_prep.md
@@ -2,6 +2,22 @@
 
 ## Unknown -- How Faker works 
 
+https://python.plainenglish.io/generating-a-fake-database-with-python-8523bf6db9ec
+
+Do I need table or S3:// storage ?
+
+### Plan on Faker Working 
+
+I want to start with 4-5 customers only for the month of Jan
+
+I would have a dictionary that would map customer_id to name 
+
+1 -> Syd ( Trial - acquired -- same tier long time )
+2 -> Roger ( trial - acquired basic -- cancelled - came back)
+3 -> David ( trial - acquired basic -- downgraded ad-supported -- upgraded basic )
+4 -> Nick ( trial - acquired basic -- upgraded to premium -- downgraded to ad-supported - upgraded to basic )
+5 -> Richard (trial - acquired basic - cancelled -- CHURN)
+
 ## End Goal 
 
 **A transaction Table** 
--- data_using_faker/event_data_faker.py
+++ data_using_faker/event_data_faker.py
@@ -1,17 +1,37 @@
-import pandas as pd 
-from faker import Faker 
+from faker import Faker
+import random
+import datetime 
 
-def faker_transaction_event_create():
-    faker_obj = Faker()
-    faker_date = faker_obj.date()
-    return faker_date
+# Set the seed value
+random.seed(1234)
 
+# Create an instance of the Faker class
+fake = Faker()
+Faker.seed(0)
 
+# A dictionary mapping customer_id to name
 
-if __name__ == "__main__":
+customer_name_dict = {
+    1: "Syd",
+    2: "Roger",
+    3: "David",
+    4: "Nick",
+    5: "Richard"
+}
 
-    # create fake event
+product_sku_dict = {
+    1: "TRIAL_00",
+    2:  "BASIC_9_99",
+    3: "PREMIUM_14_99",
+    4: "AD_SUPRT_5_99"
+}
+# TODO Learning - faker date_time_between takes datetime.date
+# TODO Learning using customer_name from dict 
+start_date = datetime.date(year=2023, month=1, day=1)
+end_date = datetime.date(year=2023, month=1, day=31)
+# Generate a list of tuples with fake data
+dataset = [(fake.uuid4(),  customer_name_dict[x],  product_sku_dict[fake.random_int(min=1, max=4)], fake.date_time_between(start_date=start_date, end_date=end_date)) for x in range(1,6)]
 
-    print(f" fake date is {faker_transaction_event_create()}")
-
+# Print the generated dataset
+print(dataset)
     
